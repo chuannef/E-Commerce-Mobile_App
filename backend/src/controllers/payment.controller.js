ad9af2ed4b2ace1d58bd5873ad305361215b2aa6
unknown <hoangchuanks@gmail.com>
2026-06-16 20:41:11 +0700

    remove unused payment import

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -3,7 +3,6 @@ import { ENV } from "../config/env.js";
 import { User } from "../models/user.model.js";
 import { Product } from "../models/product.model.js";
 import { Order } from "../models/order.model.js";
-import { Cart } from "../models/cart.model.js";
 
 const stripe = new Stripe(ENV.STRIPE_SECRET_KEY);
 
